be9491791943d7f995f217b9ed4d7e0b96d3e0ab
Bas Dijkstra <bas@ontestautomation.com>
2021-04-02 14:48:05 +0200

    Initial commit

---
+++ src/main/java/GraphQLQuery.java
@@ -0,0 +1,24 @@
+public class GraphQLQuery {
+
+    private String query;
+    private Object variables;
+
+    public GraphQLQuery() {
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public Object getVariables() {
+        return variables;
+    }
+
+    public void setVariables(Object variables) {
+        this.variables = variables;
+    }
+}
---
+++ src/test/java/BasicGraphQLTest.java
@@ -0,0 +1,76 @@
+import io.restassured.http.ContentType;
+import org.junit.*;
+
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class BasicGraphQLTest {
+
+    @Test
+    public void getCompanyData_checkCeo_shouldBeElonMusk() {
+
+        GraphQLQuery query = new GraphQLQuery();
+        query.setQuery("{ company { ceo coo name } }");
+
+        given().
+            contentType(ContentType.JSON).
+            body(query).
+        when().
+            post("https://api.spacex.land/graphql/").
+        then().
+            assertThat().
+            statusCode(200).
+        and().
+            body("data.company.ceo", equalTo("Elon Musk"));
+    }
+
+    @Test
+    public void getLaunches_checkMissionName_shouldBeThaicom6() {
+
+        GraphQLQuery query = new GraphQLQuery();
+        query.setQuery("query getLaunches($limit: Int!){ launches(limit: $limit) { mission_name } }");
+
+        QueryLimit queryLimit = new QueryLimit(10);
+        query.setVariables(queryLimit);
+
+        given().
+            contentType(ContentType.JSON).
+            body(query).
+        when().
+            post("https://api.spacex.land/graphql/").
+        then().
+            assertThat().
+            statusCode(200).
+        and().
+            body("data.launches[0].mission_name", equalTo("Thaicom 6"));
+    }
+
+    @Test
+    public void addUser_checkReturnedData_shouldCorrespondToDataSent() {
+
+        GraphQLQuery query = new GraphQLQuery();
+        query.setQuery("mutation insert_users ($id: uuid!, $name: String!, $rocket: String!) { insert_users(objects: {id: $id, name: $name, rocket: $rocket}) { returning { id name rocket } } }");
+
+        User myUser = new User(
+                UUID.randomUUID(),
+                "Bas",
+                "My awesome rocket"
+        );
+        query.setVariables(myUser);
+
+        given().
+            contentType(ContentType.JSON).
+            body(query).
+        when().
+            post("https://api.spacex.land/graphql/").
+        then().
+            assertThat().
+            statusCode(200).
+        and().
+            body("data.insert_users.returning[0].id", equalTo(myUser.getId().toString())).
+            body("data.insert_users.returning[0].name", equalTo(myUser.getName())).
+            body("data.insert_users.returning[0].rocket", equalTo(myUser.getRocket()));
+    }
+}
