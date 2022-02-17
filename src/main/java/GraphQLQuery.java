3bda371ef78fc2597cb1e35852276977d852fdd8
Bas Dijkstra <bas@ontestautomation.com>
2022-02-17 16:54:28 +0100

    Remove Lombok as a dependency and add explicit getters and setters

--- src/main/java/GraphQLQuery.java
+++ src/main/java/GraphQLQuery.java
@@ -1,8 +1,21 @@
-import lombok.Data;
-
-@Data
 public class GraphQLQuery {
 
     private String query;
     private Object variables;
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public Object getVariables() {
+        return variables;
+    }
+
+    public void setVariables(Object variables) {
+        this.variables = variables;
+    }
 }
--- src/main/java/QueryLimit.java
+++ src/main/java/QueryLimit.java
@@ -1,7 +1,12 @@
-import lombok.Data;
-
-@Data
 public class QueryLimit {
 
     private int limit;
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
 }
